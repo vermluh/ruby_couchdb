3e7060116e935402680dfa271d40d38da2d2b2db
Florian Simon <github@verm-luh.de>
2015-01-21 09:08:46 +0100

    no more bad characters :-)

--- demo_mp_json.rb
+++ demo_mp_json.rb
@@ -7,14 +7,14 @@ require 'json/ext'
 
 def read_parse_insert()
   json_mps = File.open("mp_log.json").read          # read measurements from file
-  json_mps.gsub!(/[\n\r]/, "")                      # remove bad characters
   json_mps = "[" + json_mps.gsub("}{", "},{") + "]" # transform to JSON array
   mp = JSON.parse json_mps
   db = CouchRest.database! "http://127.0.0.1:5984/messungen"
   mp.each do |messung|
     response = db.save_doc(messung)
-    ap response
+    #ap response
   end
+  puts "inserted #{mp.length} measurements"
 end
 
 puts "operation took #{Benchmark.realtime { read_parse_insert } } seconds"
